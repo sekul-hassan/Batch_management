888e127a786eefbf0e8b9d7178aa411767d9aaf5
sekul hassan <sekulhassan3031@gmail.com>
2024-05-15 18:15:30 +0600

    project structure done

---
+++ src/Components/HomeTop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Container, Row} from "react-bootstrap";
+
+function HomeTop(props) {
+    return (
+        <Container fluid="true" className="topBackground">
+            <Row className="topOverlay">
+                <div className="contentArea">
+                    <h4 className="title2 text-white">Welcome to Department of Computer Science of Engineering</h4>
+                    <h3 className="title text-white">Jahangirnagar University</h3>
+                </div>
+            </Row>
+        </Container>
+    );
+}
+
+export default HomeTop;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import HomeTop from "../Components/HomeTop";
+
+function Home(props) {
+    return (
+      <Fragment>
+          <HomeTop/>
+      </Fragment>
+    );
+}
+
+export default Home;
