57a817804bc5e045cc210568bce604b26088ee02
sekul hassan <sekulhassan3031@gmail.com>
2024-05-21 14:32:56 +0600

    modification in navbar

--- src/Components/HomeTop.js
+++ src/Components/HomeTop.js
@@ -6,7 +6,7 @@ function HomeTop(props) {
         <Container fluid="true" className="topBackground">
             <Row className="topOverlay">
                 <div className="contentArea">
-                    <h4 className="title2 text-white">Welcome to Department of Computer Science of Engineering</h4>
+                    <h4 className="title2 text-white">Welcome to Department Computer Science and Engineering</h4>
                     <h3 className="title text-white">Jahangirnagar University</h3>
                 </div>
             </Row>
