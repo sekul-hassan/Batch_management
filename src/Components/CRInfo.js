379f7ca4c17ee7068525212407c4f61939be1167
sekul hassan <sekulhassan3031@gmail.com>
2024-05-28 15:57:16 +0600

    cr design completed

--- src/Components/CRInfo.js
+++ src/Components/CRInfo.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import {Col, Container, Row} from "react-bootstrap";
+import React, {Fragment} from 'react';
+import {Button, Col, Container, Row} from "react-bootstrap";
 import honey from "../Assets/Images/honey.jpg"
 import HusbandHoney from "../Assets/Images/husbandHoney.jpg"
 
 function CrInfo(props) {
+
+    const isAdmin = localStorage.getItem('isAdmin');
+
     return (
         <Container className="mt-3">
-            <h2 className="title text-center">Our cr info</h2>
+            <Row>
+                {
+                    isAdmin ? (
+                        <Fragment>
+                            <Col sm={6} md={6} lg={6}>
+                                <h2 className="title text-center">Our cr info</h2>
+                            </Col>
+                            <Col sm={6} md={6} lg={6}>
+                                <Button variant="outline-danger" className="w-25 btn-light">Update</Button>
+                                <Button  variant="outline-success" className="w-25 btn-light mx-3">Add</Button>
+                                <Button  variant="outline-info" className="w-25 btn-light">CR list</Button>
+                            </Col>
+                        </Fragment>
+                    ): (
+                        <h2 className="title text-center">Our cr info</h2>
+                    )
+                }
+            </Row>
             <Row className="mt-3">
                 <Col sm={12} md={6} lg={6}>
-                    <img className="crPic" src={honey} alt=""/>
+                <img className="crPic" src={honey} alt=""/>
                 </Col>
                 <Col sm={12} md={6} lg={6}>
                     <h2 className="title2">Speech of our honorable CR (HONEY)</h2>
